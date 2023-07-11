e674a4fb476652f45be5c433f20338fa6b5f90e3
arypte <wjs505@gmail.com>
2023-07-11 17:04:04 +0900

    db_nft_update

--- routes/nft.js
+++ routes/nft.js
@@ -38,28 +38,32 @@ router.get('/', async (req, res) => {
   });
   
   // 특정 래플 조회
-  router.get('/:id', async (req, res) => {
+router.get('/:id', async (req, res) => {
     try {
       const tokenId = parseInt(req.params.tokenId, 10);
   
-      const nft = await client.nft.findUnique({
-        where: { tokenId, },
+      const nft = await client.nft.findMany({
+        where: {
+          day: parseInt(day, 10),
+          type: parseInt(type , 10),
+        },
       });
       return res.json(nft);
     } catch (error) {
       console.error(error);
       res.status(500).json({ error: 'An error occurred' });
     }
-  });
+});
   
-  // nft 완료
-  router.put('/done', async (req, res) => {
+  // nft 사용 완료
+router.put('/done', async (req, res) => {
     try {
-      const { tokenId } = req.body;
+      const { day , type } = req.body;
   
-      const nft = await client.nft.findUnique({
+      const nft = await client.nft.findMany({
         where: {
-          tokenId: parseInt(tokenId),
+          day: parseInt(day, 10),
+          type: parseInt(type , 10),
         },
       });
   
@@ -67,9 +71,10 @@ router.get('/', async (req, res) => {
         return res.status(400).json({ ok: false, error: "Not exist nft" });
       }
   
-      await client.nft.update({
+      await client.nft.updateMany({
         where: {
-            tokenId: parseInt(tokenId),
+          day: parseInt(day , 10),
+          type: parseInt(type , 10),
         },
         data: {
             isUsed : true ,
